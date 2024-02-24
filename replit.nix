f5f28cc7ceefb03fc1a49ab722fe9a5a474dd714
ducyen <duc_ttm@yahoo.com>
2024-02-24 08:02:20 +0000

    Added libpng

--- replit.nix
+++ replit.nix
@@ -1,8 +1,9 @@
 { pkgs }: {
 	deps = [
-   pkgs.unzip
-   pkgs.zulu8
-		pkgs.clang_12
+        pkgs.unzip
+        pkgs.zulu8
+        pkgs.imagemagick6
+        pkgs.clang_12
 		pkgs.ccls
 		pkgs.gdb
 		pkgs.gnumake
